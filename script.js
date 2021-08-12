3bc48cf24c4f8bb12fdd4416d7602b3c91364c17
wallanmota <wallanmota.13@gmail.com>
2021-08-11 23:11:18 -0300

    funcao validacao / alert

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("JavaScript carregado")
+
+function validacao(){
+    console.log("iniciando validação")
+    document.getElementById("success").style.display = "none"
+    document.getElementById("error").style.display = "none"
+
+    let cpf = document.getElementById("cpf_digitado").value;
+    console.log(cpf);
+
+    let resultadoValidacao = validaCPF(cpf);
+
+    if (resultadoValidacao){
+        document.getElementById("success").style.display = "block";
+    }
+    else{
+        document.getElementById("error").style.display = "block";
+    }
+}
